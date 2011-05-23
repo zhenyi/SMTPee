0e97ccf3b87c475d66ad868c0557b4e78bf72d00
Zhenyi Tan <qqzhenyi@gmail.com>
2011-05-23 15:50:55 +0800

    Initial Commit

---
+++ Sender/SenderAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  SenderAppDelegate.h
+//  Sender
+//
+//  Created by Zhenyi Tan on 5/23/11.
+//  Copyright 2011 And a Dinosaur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SenderAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ Sender/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Sender
+//
+//  Created by Zhenyi Tan on 5/23/11.
+//  Copyright 2011 And a Dinosaur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
